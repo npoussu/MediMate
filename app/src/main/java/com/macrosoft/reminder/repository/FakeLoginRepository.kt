02502156b488ad9a4f513ee6b4fc6b90b025d6eb
npoussu <t5poni00@students.oamk.fi>
2019-04-04 11:36:26 +0100

    - Added KDocs to FakeLoginRepository & LoginViewModel

--- app/src/main/java/com/macrosoft/reminder/repository/FakeLoginRepository.kt
+++ app/src/main/java/com/macrosoft/reminder/repository/FakeLoginRepository.kt
@@ -4,6 +4,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.macrosoft.reminder.model.User
 
+/**
+ * Mock version of the LoginRepository, can be used to test the authentication
+ */
 object FakeLoginRepository {
 
     private val _fakeUser = MutableLiveData<User>()
--- app/src/main/java/com/macrosoft/reminder/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/macrosoft/reminder/viewmodel/LoginViewModel.kt
@@ -7,10 +7,16 @@ import androidx.lifecycle.MutableLiveData
 import com.macrosoft.reminder.model.User
 import com.macrosoft.reminder.repository.FakeLoginRepository
 
+/**
+ * LoginViewModel: Holds the references to Data Binded fields at LoginFragment
+ * Handles authenticating the user
+ */
+
 class LoginViewModel : ObservableViewModel() {
 
     private val TAG = LoginViewModel::class.java.simpleName
 
+    // Data binded fields at fragment_login.xml
     @Bindable
     val userIdContent = MutableLiveData<String>()
 
@@ -20,6 +26,7 @@ class LoginViewModel : ObservableViewModel() {
     val fakeUser: LiveData<User>
         get() = FakeLoginRepository.fakeUser
 
+    // TODO: Add the user authentication function here
     fun onLoginClick() {
         Log.i(TAG, userIdContent.value)
         Log.i(TAG, passwordContent.value)
