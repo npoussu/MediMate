85d609c7541b01ce5eb06d3c5889a6657a07fa48
Kai O'Leary <c15354951@mydit.ie>
2019-04-07 18:18:15 +0200

    Comment out testing code in MainActivity

--- app/src/main/java/com/macrosoft/reminder/MainActivity.kt
+++ app/src/main/java/com/macrosoft/reminder/MainActivity.kt
@@ -15,22 +15,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val db = Room.databaseBuilder(
-            applicationContext,
-            AppDatabase::class.java, "database"
-        ).allowMainThreadQueries().build()
-
-        val user = User(1, "Joe Bob", "1234", "Joe")
-        val med = MedicineData(1, 1,"Pill", "A pill")
-        val med2 = MedicineData(2, 1,"Pill 2", "A pill 2")
-
-        db.userDao().delete(user)
-        db.userDao().insert(user)
-        db.medicineDataDAO().insert(med)
-        db.medicineDataDAO().insert(med2)
-
-        val users = db.userDao().getUserByName(user.userName)
-
-        Log.d("MainTag", users.toString())
+//        val db = Room.databaseBuilder(
+//            applicationContext,
+//            AppDatabase::class.java, "database"
+//        ).allowMainThreadQueries().build()
+//
+//        val user = User(1, "Joe Bob", "1234", "Joe")
+//        val med = MedicineData(1, 1,"Pill", "A pill")
+//        val med2 = MedicineData(2, 1,"Pill 2", "A pill 2")
+//
+//        db.userDao().delete(user)
+//        db.userDao().insert(user)
+//        db.medicineDataDAO().insert(med)
+//        db.medicineDataDAO().insert(med2)
+//
+//        val users = db.userDao().getUserByName(user.userName)
+//
+//        Log.d("MainTag", users.toString())
     }
 }
