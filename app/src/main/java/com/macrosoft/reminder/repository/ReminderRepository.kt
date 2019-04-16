beafdf1489cead6d896eab035382dcc28a740a4c
Kai O'Leary <c15354951@mydit.ie>
2019-04-17 01:05:53 +0200

    Init MedicineData and Reminder Repos

--- app/src/main/java/com/macrosoft/reminder/repository/ReminderRepository.kt
+++ app/src/main/java/com/macrosoft/reminder/repository/ReminderRepository.kt
@@ -1,2 +1,29 @@
 package com.macrosoft.reminder.repository
 
+import androidx.lifecycle.LiveData
+import com.macrosoft.reminder.database.ReminderDAO
+import com.macrosoft.reminder.model.Reminder
+
+class ReminderRepository(reminderDAO: ReminderDAO) {
+    private val reminderDAOImpl: ReminderDAO = reminderDAO
+
+    fun getRedminderByID(id: Int): LiveData<Reminder> {
+        return reminderDAOImpl.getReminderByID(id)
+    }
+
+    fun getReminderByUserID(user_id: Int): LiveData<Array<Reminder>> {
+        return reminderDAOImpl.getReminderByUserID(user_id)
+    }
+
+    fun deleteReminder(reminder: Reminder) {
+        return reminderDAOImpl.delete(reminder)
+    }
+
+    fun insertReminder(reminder: Reminder) {
+        return reminderDAOImpl.insert(reminder)
+    }
+
+    fun updateReminder(reminder: Reminder) {
+        return reminderDAOImpl.update(reminder)
+    }
+}
