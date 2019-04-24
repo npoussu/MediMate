a831a305bbea28fab36af64a2b36d68338480eda
npoussu <t5poni00@students.oamk.fi>
2019-04-24 23:03:43 +0100

    - Added a custom toolbar with layout files    - Included at the parent Activities - Added changing the title on Fragment change - Added string resources for all the titles - Added styling for toolbar

--- app/src/main/java/com/macrosoft/reminder/view/ui/loggedin/MainActivity.kt
+++ app/src/main/java/com/macrosoft/reminder/view/ui/loggedin/MainActivity.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentTransaction
 import com.macrosoft.reminder.R
+import kotlinx.android.synthetic.main.toolbar_main.*
 
 class MainActivity : AppCompatActivity(), ViewMedicineFragment.OnMedicineCardClickedListener {
 
     override fun onCreateAccountClicked() {
         val ft: FragmentTransaction = supportFragmentManager.beginTransaction()
+
+        toolbar_title.text = getString(R.string.my_medicines_title)
+
         with(ft) {
             replace(R.id.fragment_holder, ViewMedicineDetailsFragment())
             addToBackStack(null)
@@ -19,6 +23,11 @@ class MainActivity : AppCompatActivity(), ViewMedicineFragment.OnMedicineCardCli
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        toolbar_title.text = getString(R.string.my_medicines_title)
 
         val ft: FragmentTransaction = supportFragmentManager.beginTransaction()
         with(ft) {
