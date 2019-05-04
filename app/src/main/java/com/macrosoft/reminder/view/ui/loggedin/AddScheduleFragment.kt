c336e90ea08ad5c10b12dea107e3d1dbcaec815f
npoussu <t5poni00@students.oamk.fi>
2019-05-04 14:44:07 +0100

    - Added a spinner with some pre-determined values for selecting how many times a day medicine should be taken - Added string-array from the spinner fetches the values

--- app/src/main/java/com/macrosoft/reminder/view/ui/loggedin/AddScheduleFragment.kt
+++ app/src/main/java/com/macrosoft/reminder/view/ui/loggedin/AddScheduleFragment.kt
@@ -5,11 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.macrosoft.reminder.R
 import com.macrosoft.reminder.databinding.ScheduleAddFragmentBinding
 import com.macrosoft.reminder.viewmodel.AddMedicineViewModel
+import kotlinx.android.synthetic.main.schedule_add_fragment.*
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class AddScheduleFragment : Fragment() {
@@ -35,6 +37,14 @@ class AddScheduleFragment : Fragment() {
     override fun onStart() {
         super.onStart()
         listener!!.setToolbarTitle(getString(R.string.add_schedule))
+
+        val arrayAdapter = ArrayAdapter.createFromResource(
+            context!!,
+            R.array.times_to_take_medicine,
+            android.R.layout.simple_spinner_dropdown_item
+        )
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = arrayAdapter
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
